e39dfb163c461b7f5ad2d4b15f3a4437ed5c9766
Ubuntu <ubuntu@ip-172-31-23-52.ap-northeast-1.compute.internal>
2018-01-23 16:34:39 +0800

    little change

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello XMing!</h1>'
+    return '<h1 align="center"><font color="red">Hello XMing!</font></h1>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('127.0.0.1', 8000)
