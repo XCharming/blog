86cbe259b4cb0ca4cc624cab7d39096229dd0443
XCharming <xmingx@outlook.com>
2018-01-25 15:35:16 +0800

    little change

--- hello.py
+++ hello.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1 align="center"><font color="red">Hello XMing!</font></h1>'
+    return '<h1 align="center"><font color="red">Hello World!</font></h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1 align="center"><font color="red">Hello, %s!</font></h1>' % name
 
 if __name__ == '__main__':
     app.run(debug=True)
