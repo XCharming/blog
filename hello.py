bb4ba0d8a537578fb83467d959969f0cb33b7d77
Ubuntu <ubuntu@ip-172-31-23-52.ap-northeast-1.compute.internal>
2018-02-07 13:54:49 +0800

    little change

--- hello.py
+++ hello.py
@@ -1,14 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'XCharming'
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1 align="center"><font color="red">Hello World!</font></h1>'
+	#name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		#form.name.data = ''
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form,
+						   name=session.get('name'),
+						   current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1 align="center"><font color="red">Hello, %s!</font></h1>' % name
+	return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+	manager.run()
